cfec5062f8e1e1ead004e0efe237ba3f6dcb489d
sandeep10704 <saivenkatasandeep3@gmail.com>
2026-01-31 13:43:17 +0530

    putting api key in evn file

--- app/core/llm.py
+++ app/core/llm.py
@@ -1,8 +1,13 @@
 from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
 
+load_dotenv() 
+
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
 llm = ChatOpenAI(
     model="arcee-ai/trinity-large-preview:free",
     temperature=0.3,
     openai_api_base="https://openrouter.ai/api/v1",
-    openai_api_key="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"
+    openai_api_key=OPENAI_API_KEY
 )
